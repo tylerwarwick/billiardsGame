aa7a1cf8ce99aee16c68ac3ef0341d00bfb21f3a
tylerwarwick <tylerwarwickwork@gmail.com>
2024-03-27 22:17:29 -0400

    Specutively Done :)

--- server/test.sh
+++
@@ -1,2 +0,0 @@
-curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -d "player1=Tyler&player2=Erin" http://localhost:50124/newGame
-curl -X POST -H "Content-Type: application/json" -d "{\"gameId\":\"1\", \"xVel\":\"0.0\", \"yVel\":\"-1000.00\"}" http://localhost:50124/shoot
